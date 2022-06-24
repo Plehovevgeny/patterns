60d73180bbfc72b34509a73cf295df43db6bd2b5
Plehov Evgeny <plehov1992@yandex.ru>
2022-06-24 18:45:45 +0500

    change test

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -3,6 +3,7 @@ package ru.netology;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
@@ -26,6 +27,7 @@ public class CardDeliveryTest {
     public void luckyPath() {
         DataUser user = UserGenerator.generateUser(6);
         form.$x(".//input[@placeholder=\"Город\"]").val(user.getCity());
+        form.$x(".//span[@data-test-id='date']//input[@class='input__control']").doubleClick().sendKeys(Keys.BACK_SPACE);
         form.$x(".//span[@data-test-id='date']//input[@class='input__control']").val(user.getDate());
         form.$x(".//input[@name=\"name\"]").val(user.getName());
         form.$x(".//input[@name=\"phone\"]").val(user.getPhone());
@@ -38,6 +40,7 @@ public class CardDeliveryTest {
         success.$x(".//button").click();
         success.should(hidden);
 
+        form.$x(".//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(Keys.BACK_SPACE);
         form.$x(".//input[@placeholder=\"Дата встречи\"]").val(UserGenerator.generateDate(5));
         form.$x(".//button//ancestor::span[contains(text(), 'Запланировать')]").click();
 
