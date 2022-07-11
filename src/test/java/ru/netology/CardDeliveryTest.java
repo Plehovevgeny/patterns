b92e7b13c820f94680842ee5f66ba4a4f14d13ce
Plehov Evgeny <plehov1992@yandex.ru>
2022-07-11 19:12:04 +0500

    Add allure

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,6 +1,10 @@
 package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -17,6 +21,15 @@ public class CardDeliveryTest {
     SelenideElement replan = $x("//div[@data-test-id='replan-notification']");
     SelenideElement error = $x("//div[@data-test-id='error-notification']");
 
+    @BeforeAll
+    static void setUpAll(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     public void setup() {
